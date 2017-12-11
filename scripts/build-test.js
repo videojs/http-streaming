de180debf785af9aaa86d61141b340ff805ebd9c
jforbes <jforbes@brightcove.com>
2017-12-11 14:27:59 -0500

    run only the vhs test file

--- scripts/build-test.js
+++ scripts/build-test.js
@@ -2,7 +2,7 @@ var browserify = require('browserify');
 var fs = require('fs');
 var glob = require('glob');
 
-glob('test/**/*.test.js', function(err, files) {
+glob('test/**/videojs-http-streaming.test.js', function(err, files) {
   browserify(files)
     .transform('babelify')
     .transform('browserify-shim', {global: true})
