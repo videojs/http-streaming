e21e1cb8eaf780fe365e6196605a4699baec9a98
jforbes <jforbes@brightcove.com>
2018-03-14 11:14:49 -0400

    test: Added end to end playback tests for DASH

--- test/playback.test.js
+++ test/playback.test.js
@@ -81,3 +81,44 @@ QUnit.test('Advanced Bip Bop preload=none', function(assert) {
   });
 });
 
+QUnit.test('Big Buck Bunny', function(assert) {
+  let done = assert.async();
+
+  assert.expect(2);
+  let player = this.player;
+
+  player.autoplay(true);
+
+  playFor(player, 2, function() {
+    assert.ok(true, 'played for at least two seconds');
+    assert.equal(player.error(), null, 'has no player errors');
+
+    done();
+  });
+
+  player.src({
+    src: 'https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps.mpd',
+    type: 'application/dash+xml'
+  });
+});
+
+QUnit.test('Live DASH', function(assert) {
+  let done = assert.async();
+
+  assert.expect(2);
+  let player = this.player;
+
+  player.autoplay(true);
+
+  playFor(player, 2, function() {
+    assert.ok(true, 'played for at least two seconds');
+    assert.equal(player.error(), null, 'has no player errors');
+
+    done();
+  });
+
+  player.src({
+    src: 'https://vm2.dashif.org/livesim/mup_30/testpic_2s/Manifest.mpd',
+    type: 'application/dash+xml'
+  });
+});
