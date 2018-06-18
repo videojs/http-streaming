3ecc011f379fd313d92b8f461c492be25220f1d2
Garrett Singer <gesinger@gmail.com>
2018-06-18 16:53:15 -0400

    Fix segment test

--- test/util/segment.test.js
+++ test/util/segment.test.js
@@ -6,7 +6,7 @@ QUnit.module('util/segment');
 QUnit.test('concats Uint8Array bytes of segments together', function(assert) {
   const arr1 = new Uint8Array(1);
   const arr2 = new Uint8Array(2);
-  const arr3 = new Uint8Array(3);
+  const arr3 = new Uint8Array(1);
   let result;
 
   arr1[0] = 12;
@@ -19,7 +19,7 @@ QUnit.test('concats Uint8Array bytes of segments together', function(assert) {
     segments: [arr1, arr2, arr3]
   });
 
-  assert.strictEqual(result.length, 3);
+  assert.strictEqual(result.length, 4);
   assert.strictEqual(result[0], 12);
   assert.strictEqual(result[1], 4);
   assert.strictEqual(result[2], 7);
