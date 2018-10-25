ee594e5d5c36b292323071ebb2b8f472a2c840a4
Gary Katsevman <git@gkatsev.com>
2018-10-25 12:53:36 -0400

    fix(subtitles): set default property if default and autoselect are both enabled (#239)

--- src/media-groups.js
+++ src/media-groups.js
@@ -512,7 +512,7 @@ export const initialize = {
           const track = tech.addRemoteTextTrack({
             id: variantLabel,
             kind: 'subtitles',
-            enabled: false,
+            default: properties.default && properties.autoselect,
             language: properties.language,
             label: variantLabel
           }, false).track;
@@ -568,7 +568,7 @@ export const initialize = {
           const track = tech.addRemoteTextTrack({
             id: properties.instreamId,
             kind: 'captions',
-            enabled: false,
+            default: properties.default && properties.autoselect,
             language: properties.language,
             label: variantLabel
           }, false).track;
