982bf08d335e29e5e3cb4d6e66da9f81ce1ca05f
ldayananda <ldayananda@brightcove.com>
2018-12-14 18:54:26 -0500

    use real browser for Chrome

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -42,6 +42,10 @@ module.exports = function(config) {
         base: 'ChromeHeadless',
         flags: [ '--no-sandbox' ]
       },
+      ChromeWithFlags: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      },
       ChromeBrowserStack: {
         base: 'BrowserStack',
         flags: [ '--no-sandbox' ],
@@ -87,7 +91,7 @@ module.exports = function(config) {
 
         var newBrowsers = [];
         if (browsers.indexOf('Chrome') !== -1) {
-          newBrowsers.push('ChromeHeadlessWithFlags');
+          newBrowsers.push('ChromeWithFlags');
         }
 
         if (browsers.indexOf('Firefox') !== -1) {
