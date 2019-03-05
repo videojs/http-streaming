a4c056ec0f2d4a86e1b1aac48faa63fdee1b42e5
Brandon Casey <BrandonOCasey@users.noreply.github.com>
2019-03-05 15:04:55 -0500

    fix: only fire seekablechange when values of seekable ranges actually change (#415)

--- src/master-playlist-controller.js
+++ src/master-playlist-controller.js
@@ -933,6 +933,14 @@ export class MasterPlaylistController extends videojs.EventTarget {
       }
     }
 
+    let oldEnd;
+    let oldStart;
+
+    if (this.seekable_ && this.seekable_.length) {
+      oldEnd = this.seekable_.end(0);
+      oldStart = this.seekable_.start(0);
+    }
+
     if (!audioSeekable) {
       // seekable has been calculated based on buffering video data so it
       // can be returned directly
@@ -948,6 +956,13 @@ export class MasterPlaylistController extends videojs.EventTarget {
         (audioSeekable.end(0) < mainSeekable.end(0)) ? audioSeekable.end(0) :
                                                        mainSeekable.end(0)
       ]]);
+    }
+
+    // seekable is the same as last time
+    if (this.seekable_ && this.seekable_.length) {
+      if (this.seekable_.end(0) === oldEnd && this.seekable_.start(0) === oldStart) {
+        return;
+      }
     }
 
     this.logger_(`seekable updated [${Ranges.printableRange(this.seekable_)}]`);
