c46af0b59295aed2d93e2ce1eb35adfb4cfa0e0a
brandonocasey <brandonocasey@gmail.com>
2019-05-02 15:47:24 -0400

    media group changes

--- src/media-groups.js
+++ src/media-groups.js
@@ -367,7 +367,7 @@ export const initialize = {
       hls,
       sourceType,
       segmentLoaders: { [type]: segmentLoader },
-      requestOptions: { withCredentials },
+      requestOptions,
       master: { mediaGroups, playlists },
       mediaTypes: {
         [type]: {
@@ -415,11 +415,11 @@ export const initialize = {
         if (properties.resolvedUri) {
           playlistLoader = new PlaylistLoader(properties.resolvedUri,
                                               hls,
-                                              withCredentials);
+                                              requestOptions);
         } else if (properties.playlists && sourceType === 'dash') {
           playlistLoader = new DashPlaylistLoader(properties.playlists[0],
                                                   hls,
-                                                  withCredentials,
+                                                  requestOptions,
                                                   masterPlaylistLoader);
         } else {
           // no resolvedUri means the audio is muxed with the video when using this
@@ -467,7 +467,7 @@ export const initialize = {
       hls,
       sourceType,
       segmentLoaders: { [type]: segmentLoader },
-      requestOptions: { withCredentials },
+      requestOptions,
       master: { mediaGroups },
       mediaTypes: {
         [type]: {
@@ -502,11 +502,11 @@ export const initialize = {
 
         if (sourceType === 'hls') {
           playlistLoader =
-            new PlaylistLoader(properties.resolvedUri, hls, withCredentials);
+            new PlaylistLoader(properties.resolvedUri, hls, requestOptions);
         } else if (sourceType === 'dash') {
           playlistLoader = new DashPlaylistLoader(properties.playlists[0],
                                                   hls,
-                                                  withCredentials,
+                                                  requestOptions,
                                                   masterPlaylistLoader);
         }
 
