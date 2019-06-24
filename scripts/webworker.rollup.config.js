22bf51c35e979f2e54beb1ad50f904d4b457b32d
Brandon Casey <2381475+brandonocasey@users.noreply.github.com>
2019-06-24 13:48:13 -0400

    chore: faster dev build (#555)

--- scripts/webworker.rollup.config.js
+++ scripts/webworker.rollup.config.js
@@ -1,7 +1,12 @@
 import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
+import path from 'path';
+import presetEnv from 'babel-preset-env';
+import externalHelpers from 'babel-plugin-external-helpers';
 
+/* to prevent going into a screen during rollup */
+process.stderr.isTTY = false;
 export default [{
   input: 'src/decrypter-worker.js',
   output: {
@@ -18,7 +23,21 @@ export default [{
     commonjs({
       sourceMap: false
     }),
-    babel()
+    babel({
+      babelrc: false,
+      exclude: path.join(process.cwd(), 'node_modules/**'),
+      presets: [
+        [presetEnv, {
+          loose: true,
+          modules: false,
+          exclude: ['transform-es2015-typeof-symbol'],
+          targets: {
+            browsers: ['defaults', 'ie 11']
+          }
+        }]
+      ],
+      plugins: [externalHelpers]
+    })
   ]
 }, {
   input: 'src/transmuxer-worker.js',
@@ -36,6 +55,20 @@ export default [{
     commonjs({
       sourceMap: false
     }),
-    babel()
+    babel({
+      babelrc: false,
+      exclude: path.join(process.cwd(), 'node_modules/**'),
+      presets: [
+        [presetEnv, {
+          loose: true,
+          modules: false,
+          exclude: ['transform-es2015-typeof-symbol'],
+          targets: {
+            browsers: ['defaults', 'ie 11']
+          }
+        }]
+      ],
+      plugins: [externalHelpers]
+    })
   ]
 }];
