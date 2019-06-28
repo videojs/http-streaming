f500fac6cb2cf2cd75ba0f9e733fbc6679958b87
brandonocasey <brandonocasey@gmail.com>
2019-06-28 14:20:15 -0400

    remove edge and ie 11

--- scripts/karma.conf.js
+++ scripts/karma.conf.js
@@ -19,6 +19,8 @@ module.exports = function(config) {
     browserstackLaunchers(defaults) {
       delete defaults.bsSafariMojave;
       delete defaults.bsSafariElCapitan;
+      delete defaults.bsEdgeWin10;
+      delete defaults.bsIE11Win10;
 
       return defaults;
     },
