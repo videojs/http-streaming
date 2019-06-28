a574f11798499ed577bbe8ae2cd58ef7e47d323d
brandonocasey <brandonocasey@gmail.com>
2019-06-28 14:49:01 -0400

    tweak numbers for firefox

--- test/playback.test.js
+++ test/playback.test.js
@@ -39,7 +39,7 @@ QUnit.module('Playback', {
 
     // uncomment these lines when deugging
     // videojs.log.level('debug');
-    // this.fixture.style = 'position: inherit;';
+    this.fixture.style = 'position: inherit;';
 
     video.setAttribute('controls', '');
     video.setAttribute('muted', '');
@@ -237,7 +237,7 @@ QUnit.test('DASH sidx', function(assert) {
 
     // Firefox sometimes won't loop if seeking directly to the duration, or to too close
     // to the duration (e.g., 10ms from duration). 100ms seems to work.
-    player.currentTime(player.duration() - 0.1);
+    player.currentTime(player.duration() - 0.5);
   });
 
   player.src({
@@ -293,7 +293,7 @@ QUnit.test('loops', function(assert) {
 
     // Firefox sometimes won't loop if seeking directly to the duration, or to too close
     // to the duration (e.g., 10ms from duration). 100ms seems to work.
-    player.currentTime(player.duration() - 0.1);
+    player.currentTime(player.duration() - 0.5);
   });
   player.play();
 });
