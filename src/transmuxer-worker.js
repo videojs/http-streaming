5b7d47abe3b8e94b4413503389b63661ec222ae8
brandonocasey <brandonocasey@gmail.com>
2019-08-07 11:39:03 -0400

    fix: import locations

--- src/transmuxer-worker.js
+++ src/transmuxer-worker.js
@@ -14,8 +14,8 @@
  * message-based interface to a Transmuxer object.
  */
 
-import fullMux from 'mux.js/lib/mp4/transmuxer';
-import partialMux from 'mux.js/lib/partial/transmuxer';
+import {Transmuxer as FullMux} from 'mux.js/lib/mp4/transmuxer';
+import PartialMux from 'mux.js/lib/partial/transmuxer';
 import {
   secondsToVideoTs,
   videoTsToSeconds
@@ -260,8 +260,8 @@ class MessageHandlers {
       this.transmuxer.dispose();
     }
     this.transmuxer = this.options.handlePartialData ?
-      new partialMux.Transmuxer(this.options) :
-      new fullMux.Transmuxer(this.options);
+      new PartialMux(this.options) :
+      new FullMux(this.options);
 
     if (this.options.handlePartialData) {
       wirePartialTransmuxerEvents(this.self, this.transmuxer);
