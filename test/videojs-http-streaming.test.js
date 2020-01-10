5c59cb445234c562eaaa55c5549a8ef88da90cce
brandonocasey <brandonocasey@gmail.com>
2020-01-10 14:00:21 -0500

    fix: race condition preventing qualityLevels from being populating (#707 from 1.x)

--- test/videojs-http-streaming.test.js
+++ test/videojs-http-streaming.test.js
@@ -3693,8 +3693,7 @@ QUnit.test(
   }
 );
 
-// TODO: This test fails intermittently. Turn on when fixed to always pass.
-QUnit.skip('populates quality levels list when available', function(assert) {
+QUnit.test('populates quality levels list when available', function(assert) {
   this.player.src({
     src: 'manifest/master.m3u8',
     type: 'application/vnd.apple.mpegurl'
