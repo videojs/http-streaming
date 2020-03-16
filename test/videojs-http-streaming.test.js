99bf80771a28f2585d7c9955c04e10fd1b777f8a
Gary Katsevman <git@gkatsev.com>
2020-03-16 16:48:38 -0400

    test: skip flaky test (#771)

--- test/videojs-http-streaming.test.js
+++ test/videojs-http-streaming.test.js
@@ -3094,7 +3094,8 @@ QUnit.test('passes useCueTags hls option to master playlist controller', functio
   videojs.options.hls = origHlsOptions;
 });
 
-QUnit.test('populates quality levels list when available', function(assert) {
+// TODO: This test fails intermittently. Turn on when fixed to always pass.
+QUnit.skip('populates quality levels list when available', function(assert) {
   this.player.src({
     src: 'manifest/master.m3u8',
     type: 'application/vnd.apple.mpegurl'
