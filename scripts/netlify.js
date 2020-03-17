077c33cdd8e9ca1c986dc0bcbd7ca348f0b9ebe8
Gary Katsevman <git@gkatsev.com>
2020-03-17 17:22:38 -0400

    updates

--- scripts/netlify.js
+++ scripts/netlify.js
@@ -1,13 +1,17 @@
 const path = require('path');
 const sh = require('shelljs');
-const deployDir = 'deploy';
 
+const deployDir = 'deploy';
 const files = [
   'node_modules/video.js/dist/video-js.css',
   'node_modules/video.js/dist/alt/video.core.js',
+  'node_modules/video.js/dist/alt/video.core.min.js',
   'node_modules/videojs-contrib-eme/dist/videojs-contrib-eme.js',
+  'node_modules/videojs-contrib-eme/dist/videojs-contrib-eme.min.js',
   'node_modules/videojs-contrib-quality-levels/dist/videojs-contrib-quality-levels.js',
-  'node_modules/d3/d3.min.js'
+  'node_modules/videojs-contrib-quality-levels/dist/videojs-contrib-quality-levels.min.js',
+  'node_modules/d3/d3.min.js',
+  'scripts/index-demo-page.js'
 ];
 
 // cleanup previous deploy
@@ -15,12 +19,12 @@ sh.rm('-rf', deployDir);
 // make sure the directory exists
 sh.mkdir('-p', deployDir);
 
-// create nested directories for the main files
+// create nested directories
 files
-.map((file) => path.dirname(file))
-.forEach((dir) => sh.mkdir('-p', path.join(deployDir, dir)));
+  .map((file) => path.dirname(file))
+  .forEach((dir) => sh.mkdir('-p', path.join(deployDir, dir)));
 
 // copy files/folders to deploy dir
 files
-.concat('dist', 'index.html', 'index.min.html', 'utils')
-.forEach((file) => sh.cp('-r', file, path.join(deployDir, file)));
+  .concat('dist', 'index.html', 'utils')
+  .forEach((file) => sh.cp('-r', file, path.join(deployDir, file)));
