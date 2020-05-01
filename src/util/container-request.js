0c153e38f01a891f85e031c3b80cc318fa0bd66c
brandonocasey <brandonocasey@gmail.com>
2020-05-01 13:36:01 -0400

    move type array concat to its own helper

--- src/util/container-request.js
+++ src/util/container-request.js
@@ -1,68 +1,67 @@
 import {detectContainerForBytes, getId3Offset} from '@videojs/vhs-utils/dist/containers';
-import {stringToBytes, toUint8} from '@videojs/vhs-utils/dist/byte-helpers';
+import {stringToBytes, concatTypedArrays} from '@videojs/vhs-utils/dist/byte-helpers';
 import {callbackWrapper} from '../xhr';
 
+// calls back if the request is readyState DONE
+// which will only happen if the request is complete.
+const callbackOnCompleted = (request, cb) => {
+  if (request.readyState === 4) {
+    return cb();
+  }
+  return;
+};
+
 const containerRequest = (uri, xhr, cb) => {
-  let bytes;
+  let bytes = [];
   let id3Offset;
   let finished = false;
+
+  const callback = function(err, req, type, _bytes) {
+    req.abort();
+    finished = true;
+    return cb(err, req, type, _bytes);
+  };
 
   const progressListener = function(error, request) {
     if (finished) {
       return;
     }
     if (error) {
-      finished = true;
-      return cb(error, request, '', bytes);
-    }
-    const currentLength = bytes && bytes.length || 0;
-    const contentPart = request.responseText.substring(currentLength, request.responseText.length);
-    const newBytes = toUint8(stringToBytes(contentPart, true));
-
-    // we use a "temp" Uint8array here because we want to set bytes
-    // to the old bytes plus the new bytes, but we don't want to go through
-    // converting the old bytes that we have from a string to bytes again.
-    // Instead we create a Uint8array and share the new/old bytes
-    // to this object. Then set bytes equal to temp.
-    const temp = new Uint8Array(currentLength + newBytes.length);
-
-    if (bytes) {
-      temp.set(bytes);
-    }
-    temp.set(newBytes, currentLength);
-    bytes = temp;
-
-    // we need at least 10 bytes to determine a type
-    if (bytes.length < 10) {
-      return;
+      return callback(error, request, '', bytes);
     }
 
+    // grap the new part of content that was just downloaded
+    const newPart = request.responseText.substring(
+      bytes && bytes.byteLength || 0,
+      request.responseText.length
+    );
+
+    // add that onto bytes
+    bytes = concatTypedArrays(bytes, stringToBytes(newPart, true));
     id3Offset = id3Offset || getId3Offset(bytes);
 
-    // if we have an id3 offset we are dealing with aac/mp3 and we
-    // need 2 bytes after the id3 offset to determine which on it is.
-    if ((id3Offset && bytes.length < id3Offset + 2)) {
-      return;
+    // we need at least 10 bytes to determine a type
+    // or we need at least two bytes after an id3Offset
+    if (bytes.length < 10 || (id3Offset && bytes.length < id3Offset + 2)) {
+      return callbackOnCompleted(request, () => callback(error, request, '', bytes));
     }
+
     const type = detectContainerForBytes(bytes);
 
     // if this looks like a ts segment but we don't have enough data
     // to see the second sync byte, wait until we have enough data
     // before declaring it ts
     if (type === 'ts' && bytes.length < 188) {
-      return;
+      return callbackOnCompleted(request, () => callback(error, request, '', bytes));
     }
 
     // this may be an unsynced ts segment
     // wait for 376 bytes before detecting no container
     if (!type && bytes.length < 376) {
-      return;
+      return callbackOnCompleted(request, () => callback(error, request, '', bytes));
     }
 
-    request.abort();
-
-    finished = true;
-    return cb(null, request, type, bytes);
+    return callback(null, request, type, bytes);
   };
 
   const options = {
@@ -70,20 +69,13 @@ const containerRequest = (uri, xhr, cb) => {
     beforeSend(request) {
       // this forces the browser to pass the bytes to us unprocessed
       request.overrideMimeType('text/plain; charset=x-user-defined');
-      request.addEventListener('progress', function(event) {
+      request.addEventListener('progress', function({total, loaded}) {
         return callbackWrapper(request, null, {statusCode: request.status}, progressListener);
       });
     }
   };
 
   const request = xhr(options, function(error, response) {
-    // if progress listeners are not supported, or
-    // for some reason we get here and are not finished,
-    // complete container checking with all of the data.
-    if (finished) {
-      return;
-    }
-
     return callbackWrapper(request, error, response, progressListener);
   });
 
