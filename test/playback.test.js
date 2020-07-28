8f9069c289dcb4c6824cd7004125c3179dbcbb78
Gary Katsevman <git@gkatsev.com>
2020-07-28 15:08:33 -0400

    chore: update DASH stream urls (#918)

--- test/playback.test.js
+++ test/playback.test.js
@@ -161,7 +161,7 @@ QUnit.test('Live DASH', function(assert) {
   });
 
   player.src({
-    src: 'https://vm2.dashif.org/livesim/mup_30/testpic_2s/Manifest.mpd',
+    src: 'https://livesim.dashif.org/livesim/mup_30/testpic_2s/Manifest.mpd',
     type: 'application/dash+xml'
   });
 });
