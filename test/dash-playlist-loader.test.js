11551ec2d84789d5e670011bb2b3ba0db442c356
brandonocasey <brandonocasey@gmail.com>
2021-05-13 12:48:26 -0400

    fix other test

--- test/dash-playlist-loader.test.js
+++ test/dash-playlist-loader.test.js
@@ -2456,7 +2456,7 @@ QUnit.test(
     );
     assert.equal(
       loader.master.mediaGroups.AUDIO.audio.main.playlists[0].uri,
-      '0-placeholder-uri-AUDIO-audio-main', 'setup phony uri for media groups'
+      'placeholder-uri-AUDIO-audio-main', 'setup phony uri for media groups'
     );
     assert.equal(
       loader.master.mediaGroups.AUDIO.audio.main.playlists[0].id,
