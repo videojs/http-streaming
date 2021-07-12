e7a39d85edf52b5878a55bd99419d522437bcde2
brandonocasey <brandonocasey@gmail.com>
2021-07-12 12:40:57 -0400

    only run test on main/fmp4

--- test/loader-common.js
+++ test/loader-common.js
@@ -822,38 +822,38 @@ export const LoaderCommonFactory = ({
       });
     });
 
-    QUnit.test('playlist change before any appends does not error', function(assert) {
-      return setupMediaSource(loader.mediaSource_, loader.sourceUpdater_).then(() => {
-        loader.playlist(playlistWithDuration(50, {
-          uri: 'bar-720.m3u8',
-          mediaSequence: 0,
-          endList: true
-        }));
-
-        loader.load();
-        this.clock.tick(1);
-        return Promise.resolve();
-      }).then(() => new Promise((resolve, reject) => {
-        loader.on('playlistupdate', () => {
-          this.clock.tick(1);
-          resolve();
-        });
-        loader.on('trackinfo', () => {
+    // only main/fmp4 segment loaders use async appends and parts/partIndex
+    if (usesAsyncAppends) {
+      QUnit.test('playlist change before any appends does not error', function(assert) {
+        return setupMediaSource(loader.mediaSource_, loader.sourceUpdater_).then(() => {
           loader.playlist(playlistWithDuration(50, {
-            uri: 'bar-1080.m3u8',
+            uri: 'bar-720.m3u8',
             mediaSequence: 0,
             endList: true
           }));
+
+          loader.load();
+          this.clock.tick(1);
+          return Promise.resolve();
+        }).then(() => new Promise((resolve, reject) => {
+          loader.on('playlistupdate', () => {
+            this.clock.tick(1);
+            resolve();
+          });
+          loader.on('trackinfo', () => {
+            loader.playlist(playlistWithDuration(50, {
+              uri: 'bar-1080.m3u8',
+              mediaSequence: 0,
+              endList: true
+            }));
+          });
+          standardXHRResponse(this.requests.shift(), tsVideoSegment());
+
+        })).then(() => {
+          assert.equal(loader.pendingSegment_.playlist.uri, 'bar-720.m3u8', 'previous playlist segment');
         });
-        standardXHRResponse(this.requests.shift(), tsVideoSegment());
-
-      })).then(() => {
-        assert.equal(loader.pendingSegment_.playlist.uri, 'bar-720.m3u8', 'previous playlist segment');
       });
-    });
 
-    // only main/fmp4 segment loaders use parts/partIndex
-    if (usesAsyncAppends) {
       QUnit.test('mediaIndex and partIndex are used', function(assert) {
         return setupMediaSource(loader.mediaSource_, loader.sourceUpdater_).then(() => {
           loader.playlist(playlistWithDuration(50, {
