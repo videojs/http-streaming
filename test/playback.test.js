98c6984b00928935a4611efd10f9aa267d717004
Gary Katsevman <git@gkatsev.com>
2021-12-10 12:59:03 -0500

    listen once

--- test/playback.test.js
+++ test/playback.test.js
@@ -227,7 +227,7 @@ QUnit[testFn]('Live DASH', function(assert) {
   const done = assert.async();
   const player = this.player;
 
-  player.on('playing', function() {
+  player.one('playing', function() {
     const firstSeekable = player.seekable();
     const firstSeekableEnd = firstSeekable.end(firstSeekable.length - 1);
 
