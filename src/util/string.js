1ff8b1a339a0c46ec5959ab7ded3d7bb5eee89f5
Adam Waldron <awaldron@brightcove.com>
2023-12-08 11:29:50 -0800

    fix bufferToHex

--- src/util/string.js
+++ src/util/string.js
@@ -2,5 +2,5 @@ export const uint8ToUtf8 = (uintArray) =>
   decodeURIComponent(escape(String.fromCharCode.apply(null, uintArray)));
 
 export const arrayBufferToHexString = (buffer) => {
-  return Array.from(buffer).map((val) => val.toString(16).padStart(2, '0')).join('');
+  return Array.from(new Uint8Array(buffer)).map((val) => val.toString(16).padStart(2, '0')).join('');
 };
